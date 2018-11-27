6aa26e45c2b919961e4cea4f9e0cb83117851b00
Spokeek <spokeek@gmail.com>
2018-11-27 18:15:19 +0100

    clean code and add test animation

---
+++ public/js/svg.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('polygon[id^=room]').forEach(p => {
+	var text = document.createElementNS("http://www.w3.org/2000/svg", 'text')
+	var center = [...p.points].reduce((acc, cur) => ({x: acc.x+(cur.x/p.points.length), y: acc.y+(cur.y/p.points.length)}), {x:0,y:0})
+	text.setAttributeNS(null, 'id', 'room-title-'+p.id)
+	text.setAttributeNS(null, 'font-size', 15)
+	text.setAttributeNS(null, 'x', center.x- 10)
+	text.setAttributeNS(null, 'y', center.y)
+	text.appendChild(document.createTextNode(p.id.substring(5)))
+	p.parentNode.appendChild(text)
+})
--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-var express = require('express')
-var app = express();
-var http = require('http').Server(app)
-var io = require('socket.io')(http);
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const fs = require('fs')
 
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use('socket.io', express.static('node_modules/socket.io-client/dist'))
 
+const svg = fs.readdirSync('svg').map(file => fs.readFileSync('svg/'+file))
+
 app.get('/', function(req, res, next){
-  res.sendFile(__dirname + '/public/index.html');
-});
+  res.render('index', {svg})
+})
 
 io.on('connection', function(socket){
-//  console.log('a user connected');
-});
-http.listen(8080, () => console.log("Serveur launched at port 8080"));
+//  console.log('a user connected')
+})
+
+http.listen(8080, () => console.log("Serveur launched at port 8080"))
