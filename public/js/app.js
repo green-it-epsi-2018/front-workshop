0ebd3315faed5cdd825f487ac02ebb9ee269c5f9
khaled44000 <fkiri44@outlook.fr>
2018-11-27 16:32:42 +0100

    Maj Fond

---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+
+var socket = io();
+
+/*affichage date aujourd'hui */
+document.getElementById('dateId').innerHTML = new Date().toLocaleDateString();
---
+++ server.js
@@ -0,0 +1,16 @@
+
+var express = require('express')
+var app = express();
+var http = require('http').Server(app)
+var io = require('socket.io')(http);
+
+app.use(express.static('public'))
+app.use('socket.io', express.static('node_modules/socket.io-client/dist'))
+
+app.get('/', function(req, res, next){
+  res.sendFile(__dirname + '/public/index.html');
+});
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+http.listen(8080);
