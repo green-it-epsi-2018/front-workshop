7d4c7f0ab8db34ec4259b010c5b2c83bbcd4d1e9
Spokeek <spokeek@gmail.com>
2018-11-28 14:05:29 +0100

    changed the logos and added the modal

--- public/js/svg.js
+++ public/js/svg.js
@@ -1,3 +1,20 @@
+const svgContainer = document.getElementById('svg-container');
+const modalDiv = document.getElementById('modal');
+
+const _changeTransitionState = (state) => {
+  svgContainer.setAttribute('class', `svg-actif-${state}`);
+}
+const _getCurrentTransitionState = () => {
+  return +svgContainer.getAttribute('class').substring(10);
+}
+
+document.addEventListener('click', (e) => {
+  console.log(e.target, modalDiv)
+  if(modalDiv.getAttribute('class') === 'opened' && e.target !== modalDiv && !modalDiv.contains(e.target)){
+    modalDiv.setAttribute('class', 'closed');
+  }
+})
+
 document.querySelectorAll('polygon[id^=room]').forEach(p => {
 	var text = document.createElementNS("http://www.w3.org/2000/svg", 'text')
 	var center = [...p.points].reduce((acc, cur) => ({x: acc.x+(cur.x/p.points.length), y: acc.y+(cur.y/p.points.length)}), {x:0,y:0})
@@ -6,17 +23,17 @@ document.querySelectorAll('polygon[id^=room]').forEach(p => {
 	text.setAttributeNS(null, 'x', center.x- 10)
 	text.setAttributeNS(null, 'y', center.y)
 	text.appendChild(document.createTextNode(p.id.substring(5)))
-	p.parentNode.appendChild(text)
+  p.parentNode.appendChild(text)
+  
+  p.addEventListener('click', (e) => {
+    const currentState = _getCurrentTransitionState();
+    if(currentState != 0 && modalDiv.getAttribute('class') === 'closed' && e.target.id.startsWith(`room-${currentState}`)){
+      console.log('you clicked on the room id '+e.target.id);
+      modalDiv.setAttribute('class', 'opened');
+      e.stopPropagation();
+    }
+  })
 })
-
-const svgContainer = document.getElementById('svg-container');
-
-const _changeTransitionState = (state) => {
-  svgContainer.setAttribute('class', `svg-actif-${state}`);
-}
-const _getCurrentTransitionState = () => {
-  return +svgContainer.getAttribute('class').substring(10);
-}
 
 const controlPrec = () => {
   const currentState = _getCurrentTransitionState();
