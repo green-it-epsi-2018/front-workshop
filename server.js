f855f3aed188b81c23feafc4edb10a6d39723685
khaled44000 <fkiri44@outlook.fr>
2018-11-28 12:53:55 +0100

    ajout demo de test

--- server.js
+++ server.js
@@ -3,6 +3,70 @@ const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const fs = require('fs')
+var date = new Date(2018, 11, 28, 8, 00, 00, 00);
+var objct = [{ 
+  "DateDebut": date.getTime(),
+  "DateFin": new Date(2018, 11, 28, 12, 00, 00).getTime(),
+  "Promo": "I5",
+  "Intervenant": "Bernard",
+  "Matiere": "pilotage de  projet",
+  "NumeroSalle": 200,
+  "NumeroEtage": 2
+},
+{
+"DateDebut": date.getTime(),
+"DateFin": new Date(2018, 11, 28, 10, 00, 00).getTime(),
+"Promo": "I4",
+"Intervenant": "Khaled",
+"Matiere": "Réseaux",
+"NumeroSalle": 214,
+"NumeroEtage": 2
+},
+{
+  "DateDebut": new Date(2018, 11, 28, 10, 00, 00).getTime(),
+  "DateFin": new Date(2018, 11, 28, 12, 00, 00).getTime(),
+  "Promo": "I4",
+  "Intervenant": "Hugo",
+  "Matiere": "IA",
+  "NumeroSalle": 214,
+  "NumeroEtage": 2
+  },
+  {
+    "DateDebut": new Date(2018, 11, 28, 14, 00, 00).getTime(),
+    "DateFin": new Date(2018, 11, 28, 17, 00, 00).getTime(),
+    "Promo": "I4",
+    "Intervenant": "Alexendre",
+    "Matiere": "Big Data",
+    "NumeroSalle": 100,
+    "NumeroEtage": 1
+  },
+  {
+    "DateDebut": new Date(2018, 11, 28, 8, 00, 00).getTime(),
+    "DateFin": new Date(2018, 11, 28, 10, 00, 00).getTime(),
+    "Promo": "I4",
+    "Intervenant": "Prof Anglais",
+    "Matiere": "Anglais",
+    "NumeroSalle": 101,
+    "NumeroEtage": 1
+  },
+  {
+    "DateDebut": new Date(2018, 11, 28, 8, 00, 00).getTime(),
+    "DateFin": new Date(2018, 11, 28, 12, 00, 00).getTime(),
+    "Promo": "I4",
+    "Intervenant": "Prof Anglais",
+    "Matiere": "Anglais",
+    "NumeroSalle": 300,
+    "NumeroEtage": 3
+  },
+  {
+    "DateDebut": new Date(2018, 11, 28, 8, 00, 00).getTime(),
+    "DateFin": new Date(2018, 11, 28, 12, 00, 00).getTime(),
+    "Promo": "I4",
+    "Intervenant": "Prof Anglais",
+    "Matiere": "Anglais",
+    "NumeroSalle": 300,
+    "NumeroEtage": 3
+  }]
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -11,8 +75,7 @@ app.use('socket.io', express.static('node_modules/socket.io-client/dist'))
 const svg = fs.readdirSync('svg').map(file => fs.readFileSync('svg/'+file))
 
 app.get('/', function(req, res, next){
-
-  res.render('index', {svg})
+  res.render('index', {svg, dataObject: objct})
 })
 
 io.on('connection', function(socket){
