31566d2e3eaec5b0fda495971a7e9dd254635bf5
Spokeek <spokeek@gmail.com>
2018-11-28 16:40:03 +0100

    ajout du calendrier

--- public/js/svg.js
+++ public/js/svg.js
@@ -1,15 +1,18 @@
 const svgContainer = document.getElementById('svg-container');
 const modalDiv = document.getElementById('modal');
 
+const roomsIdList = [...document.querySelectorAll('polygon[id^=room]')].map(n => n.id)
+
 const _changeTransitionState = (state) => {
-  svgContainer.setAttribute('class', `svg-actif-${state}`);
+  if(modalDiv.getAttribute('class') === 'closed'){
+    svgContainer.setAttribute('class', `svg-actif-${state}`);
+  }
 }
 const _getCurrentTransitionState = () => {
   return +svgContainer.getAttribute('class').substring(10);
 }
 
 document.addEventListener('click', (e) => {
-  console.log(e.target, modalDiv)
   if(modalDiv.getAttribute('class') === 'opened' && e.target !== modalDiv && !modalDiv.contains(e.target)){
     modalDiv.setAttribute('class', 'closed');
   }
@@ -28,13 +31,12 @@ document.querySelectorAll('polygon[id^=room]').forEach(p => {
   p.addEventListener('click', (e) => {
     const currentState = _getCurrentTransitionState();
     if(currentState != 0 && modalDiv.getAttribute('class') === 'closed' && e.target.id.startsWith(`room-${currentState}`)){
-      console.log('you clicked on the room id '+e.target.id);
       modalDiv.setAttribute('class', 'opened');
+      setModalContent(e.target.id);
       e.stopPropagation();
     }
   })
 })
-
 const controlPrec = () => {
   const currentState = _getCurrentTransitionState();
   if(currentState > 1){
@@ -50,4 +52,35 @@ const controlSuiv = () => {
 }
 const controlAcceuil = () => {
   _changeTransitionState(0);
+}
+
+const setModalContent = (idRoom) => {
+  const numeroSalle = +idRoom.substring(5);
+  for(var y = 0; y < (20 - 8); y++){
+    for(var x = 0; x < 5; x++){
+      let currDate = new Date()
+      currDate = new Date(currDate.getFullYear(), currDate.getMonth(), currDate.getDate() - currDate.getDay() + 1 + x);
+      const firstMiddleDate = new Date(currDate.getFullYear(), currDate.getMonth(), currDate.getDate(), y + 8, 15, 00)
+      const secondMiddleDate = new Date(firstMiddleDate.getTime() + (30 * 60000))
+
+      const firstEvent = getEventOccupation(numeroSalle, firstMiddleDate);
+      const secondEvent = getEventOccupation(numeroSalle, secondMiddleDate)
+      modalDiv.querySelector(`tr:nth-child(${(y + 1) * 2}) td:nth-child(${2 + x})`).innerHTML = isRoomAvailable(numeroSalle, firstMiddleDate) ? "Libre" : `${firstEvent.Promo} : ${firstEvent.Matiere}`
+      modalDiv.querySelector(`tr:nth-child(${((y + 1) * 2) + 1}) td:nth-child(${2 + x})`).innerHTML = isRoomAvailable(numeroSalle, secondMiddleDate) ? "Libre" : `${secondEvent.Promo} : ${secondEvent.Matiere}`
+    }
+  }
+}
+
+const isRoomAvailable = (roomNumber, date) => {
+  const currentTimestamp = date.getTime();
+  return !events.some((event) => event.NumeroSalle === roomNumber && event.DateDebut < currentTimestamp && event.DateFin > currentTimestamp);
+}
+
+const getEventOccupation = (roomNumber, date) => {
+  const currentTimestamp = date.getTime();
+  return events.find((event) => event.NumeroSalle === roomNumber && event.DateDebut < currentTimestamp && event.DateFin > currentTimestamp);
+}
+
+const getListAvailableRooms = (date) => {
+  return roomsIdList.filter((roomId) => isRoomAvailable(+roomId.substring(5), date));
 }
--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const io = require('socket.io')(http)
 const fs = require('fs')
 var date = new Date(2018, 11, 28, 8, 00, 00, 00);
 var objct = [{ 
-  "DateDebut": date.getTime(),
-  "DateFin": new Date(2018, 11, 28, 12, 00, 00).getTime(),
+  "DateDebut": new Date(2018, 10, 28, 10, 00, 00).getTime(),
+  "DateFin": new Date(2018, 10, 28, 12, 00, 00).getTime(),
   "Promo": "I5",
   "Intervenant": "Bernard",
   "Matiere": "pilotage de  projet",
@@ -14,7 +14,7 @@ var objct = [{
   "NumeroEtage": 2
 },
 {
-"DateDebut": date.getTime(),
+  "DateDebut": new Date(2018, 10, 28, 08, 00, 00).getTime(),
 "DateFin": new Date(2018, 11, 28, 10, 00, 00).getTime(),
 "Promo": "I4",
 "Intervenant": "Khaled",
@@ -50,8 +50,8 @@ var objct = [{
     "NumeroEtage": 1
   },
   {
-    "DateDebut": new Date(2018, 11, 28, 8, 00, 00).getTime(),
-    "DateFin": new Date(2018, 11, 28, 12, 00, 00).getTime(),
+    "DateDebut": new Date(2018, 10, 28, 8, 00, 00).getTime(),
+    "DateFin": new Date(2018, 10, 28, 12, 00, 00).getTime(),
     "Promo": "I4",
     "Intervenant": "Prof Anglais",
     "Matiere": "Anglais",
@@ -59,8 +59,8 @@ var objct = [{
     "NumeroEtage": 3
   },
   {
-    "DateDebut": new Date(2018, 11, 28, 8, 00, 00).getTime(),
-    "DateFin": new Date(2018, 11, 28, 12, 00, 00).getTime(),
+    "DateDebut": new Date(2018, 10, 28, 8, 00, 00).getTime(),
+    "DateFin": new Date(2018, 10, 28, 12, 00, 00).getTime(),
     "Promo": "I4",
     "Intervenant": "Prof Anglais",
     "Matiere": "Anglais",
