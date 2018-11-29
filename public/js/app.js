565854bfb69e579de774e0400521cc0858ab75b9
khaled44000 <fkiri44@outlook.fr>
2018-11-29 11:56:40 +0100

    ajout icon favicon

--- public/js/app.js
+++ public/js/app.js
@@ -21,10 +21,10 @@ $("#slideshow > div:gt(0)").hide();
 
 setInterval(function() { 
   $('#slideshow > div:first')
-    .fadeOut(2000)
+    .fadeOut(6000)
     .next()
-    .fadeIn(2000)
+    .fadeIn(6000)
     .end()
     .appendTo('#slideshow');
-},  4000);
+},  8000);
 
